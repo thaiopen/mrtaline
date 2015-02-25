9ba717bfa9247685ac0d08c95d1f1dede818696a
Sawangpong Muadphet <sawangpong@gmail.com>
2015-02-25 12:36:10 +0700

    commit: reinstall virtualenv and crate dependencies.pip

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/admin/Django/mrtaline.com/venv"
+set -gx VIRTUAL_ENV "/home/admin/DjangoProj/development/mrtaline/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
